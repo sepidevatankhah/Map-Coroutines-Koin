35d5d378329d09f58f3c4e70969ed1bf1a86f569
Sepide <sun.vatankhah@gmail.com>
2021-07-04 02:49:06 +0200

    Added MapViewModel.kt

--- app/src/main/java/ir/nwise/app/ui/main/MainFragment.kt
+++ app/src/main/java/ir/nwise/app/ui/main/MainFragment.kt
@@ -1,80 +1,98 @@
 package ir.nwise.app.ui.main
 
-import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.target.CustomTarget
-import com.bumptech.glide.request.transition.Transition
+import android.util.Log
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import ir.nwise.app.R
+import ir.nwise.app.databinding.MainFragmentBinding
+import ir.nwise.app.domain.models.Car
+import ir.nwise.app.ui.base.BaseFragment
+import ir.nwise.app.ui.utils.getBitmap
+import ir.nwise.app.ui.utils.toastOopsError
+import org.koin.android.viewmodel.ext.android.viewModel
 
 
-class MainFragment : Fragment() {
-
+class MainFragment : BaseFragment<MapViewState, MapViewModel, MainFragmentBinding>() {
     companion object {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
-    private lateinit var mMap: GoogleMap
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val rootView = inflater.inflate(R.layout.main_fragment, container, false)
+    private val mapViewModel: MapViewModel by viewModel()
+    private var googleMap: GoogleMap? = null
 
+    override fun getLayout(): Int = R.layout.main_fragment
+
+    override var callObserverFromOnViewCreated: Boolean = false
+
+    override fun onCreateViewCompleted() {
         val mapFragment =
             childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync { googleMap ->
-            mMap = googleMap
-            val sydney = LatLng(-34.0, 151.0)
-            val germany = LatLng(40.0, 40.0)
-            val mapMarker = mMap.addMarker(
-                MarkerOptions().position(germany).title("Marker in Munich")
-            )
-            mapMarker.showInfoWindow()
-            loadMarkerIcon(mapMarker)
+            this.googleMap = googleMap
+            startObserving()
         }
-
-        return rootView
     }
 
-    private fun loadMarkerIcon(marker: Marker) {
-        val burlImg = "http://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png"
-
-        Glide.with(this)
-            .asBitmap()
-            .load(burlImg)
-            .into(object : CustomTarget<Bitmap>() {
-                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                    val icon = BitmapDescriptorFactory.fromBitmap(resource)
-                    marker.setIcon(icon)
-                }
-
-                override fun onLoadCleared(placeholder: Drawable?) {
-                    // this is called when imageView is cleared on lifecycle call or for
-                    // some other reason.
-                    // if you are referencing the bitmap somewhere else too other than this imageView
-                    // clear it here as you can no longer have the bitmap
-                }
-            })
+    override fun onCreate(savedInstanceState: Bundle?) {
+        viewModel = mapViewModel
+        super.onCreate(savedInstanceState)
+        viewModel.getCars()
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun render(state: MapViewState) {
+        when (state) {
+            is MapViewState.Loading -> {
+
+            }
+            is MapViewState.Loaded -> {
+                showMarkers(state.cars)
+            }
+            is MapViewState.Error -> {
+
+                Log.e(
+                    "MapFragment",
+                    state.throwable.message,
+                    state.throwable
+                )
+                binding.root.toastOopsError()
+            }
+        }
+    }
+
+    private fun showMarkers(vehicles: List<Car>) {
+        vehicles.forEach { vehicle ->
+            val location = LatLng(vehicle.latitude, vehicle.longitude)
+            getBitmap(requireContext(), vehicle.carImageUrl) { bitmap ->
+                googleMap?.let {
+                    val mapMarker = it.addMarker(
+                        MarkerOptions()
+                            .position(location)
+                            .icon(BitmapDescriptorFactory.fromBitmap(bitmap))
+                            .title(vehicle.make)
+                    )
+                    mapMarker.showInfoWindow()
+
+                }
+            }
+
+        }
+        zoomToLastVehicle(vehicles.last())
+    }
+
+    private fun zoomToLastVehicle(lastVehicle: Car) {
+        googleMap?.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(
+                LatLng(
+                    lastVehicle.latitude,
+                    lastVehicle.longitude
+                ), 14F
+            )
+        )
     }
 
 }
---
+++ app/src/main/java/ir/nwise/app/ui/main/MapViewModel.kt
@@ -0,0 +1,36 @@
+package ir.nwise.app.ui.main
+
+import ir.nwise.app.domain.models.Car
+import ir.nwise.app.domain.usecase.GetCarsUseCase
+import ir.nwise.app.domain.usecase.base.UseCaseResult
+import ir.nwise.app.ui.base.BaseViewModel
+
+
+class MapViewModel(private val getCarsUseCase: GetCarsUseCase) :
+    BaseViewModel<MapViewState>() {
+
+    var carList : List<Car>? = null
+    fun getCars() {
+        getCarsUseCase.execute {
+            when (this) {
+                is UseCaseResult.Loading -> liveData.postValue(MapViewState.Loading)
+                is UseCaseResult.Success ->{
+                    carList = this.data
+                    liveData.postValue(MapViewState.Loaded(this.data))
+                }
+                is UseCaseResult.Error -> liveData.postValue(MapViewState.Error(this.exception))
+            }
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        getCarsUseCase.unsubscribe()
+    }
+}
+
+sealed class MapViewState {
+    object Loading : MapViewState()
+    data class Loaded(val cars: List<Car>) : MapViewState()
+    data class Error(val throwable: Throwable) : MapViewState()
+}
