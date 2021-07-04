31ac20872487030aacc599e6d03d2df52a5d2248
Sepide <sun.vatankhah@gmail.com>
2021-07-04 02:44:21 +0200

    Added DI.

--- app/src/main/java/ir/nwise/app/di/DomainModule.kt
+++ app/src/main/java/ir/nwise/app/di/DomainModule.kt
@@ -1,13 +1,17 @@
 package ir.nwise.app.di
 
+import ir.nwise.app.data.repository.AppRepository
+import ir.nwise.app.data.repository.AppRepositoryImp
+import ir.nwise.app.domain.usecase.GetCarsUseCase
+import ir.nwise.app.networking.ApiService
 import org.koin.dsl.module
 
 val domainModule = module {
 
-//    fun provideRepository(apiService: ApiService): AppRepository {
-//        return AppRepositoryImp(apiService)
-//    }
-//
-//    factory { provideRepository(get()) }
-//    factory { GetCarsUseCase(get()) }
+    fun provideRepository(apiService: ApiService): AppRepository {
+        return AppRepositoryImp(apiService)
+    }
+
+    factory { provideRepository(get()) }
+    factory { GetCarsUseCase(get()) }
 }
--- app/src/main/java/ir/nwise/app/di/UiModule.kt
+++ app/src/main/java/ir/nwise/app/di/UiModule.kt
@@ -1,8 +1,12 @@
 package ir.nwise.app.di
 
 import ir.nwise.app.ui.main.MainFragment
+import ir.nwise.app.ui.main.MapViewModel
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val uiModule = module {
     factory { MainFragment() }
+
+    viewModel { MapViewModel(get()) }
 }
